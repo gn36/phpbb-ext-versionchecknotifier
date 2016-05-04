0a3b13d52cbdaf5a484134ea12c9d1b98c039cf6
Martin Beckmann <gn#36@phpbb.de>
2016-05-04 13:43:56 +0200

    Adjust langtest paths, remove composer install from php 5.3.3

--- travis/test-lang.sh
+++ travis/test-lang.sh
@@ -16,7 +16,7 @@ cd ../langtest ;
 
 for i in $(ls language/)
 do 
-	php vendor/bin/PhpbbTranslationValidator.php validate --language-dir=language $i
+	php ../phpBB3/vendor/bin/PhpbbTranslationValidator.php validate --language-dir=language $i
 	if [ ! $? -eq 0 ]; then
 		retval=false
 	fi
